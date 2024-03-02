eb995291b39a3c78ab0878de812eca1a1301db28
alperenkaracete <m.alperenk@gmail.com>
2024-03-03 00:00:11 +0300

    M.Substring challenge

---
+++ CppChallenges/MinWindowSubstring.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string MinWindowSubstring(string strArr[], int arrLength) {
+  
+  vecto temp;
+  // code goes here  
+  cout << arrLength << endl;
+  return strArr[0];
+
+}
+
+int main(void) { 
+   
+  // keep this function call here
+  string A[] = coderbyteInternalStdinFunction(stdin);
+  int arrLength = sizeof(A) / sizeof(*A);
+  cout << MinWindowSubstring(A, arrLength);
+  return 0;
+    
+}
