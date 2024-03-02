eadba3bdef16afff96e67fff5ea31dbad00f7fd9
alperenkaracete <m.alperenk@gmail.com>
2024-03-03 02:53:03 +0300

    Maked some changes

--- CppChallenges/MinWindowSubstring.cpp
+++ CppChallenges/MinWindowSubstring.cpp
@@ -4,10 +4,58 @@ using namespace std;
 
 string MinWindowSubstring(string strArr[], int arrLength) {
   
-  vecto temp;
+  int length = strArr[1].length();
+  int pos[length];
+  int count=0;
+  string temp;
+  int min=999999,
+    max=0;
+  for (int i=0;i<length;i++){
+
+    for (int j=0;j<strArr[0].length();j++){
+
+      if (strArr[0][j]==strArr[1][i]){
+
+        pos[i]=j;
+        for (int k=0;k<i;k++){
+
+          if(pos[k]==j){
+
+            count++;
+          }
+        }
+        if (count == 0){
+          
+          break;
+        }
+        count = 0;
+        
+      }
+      count = 0;
+
+    }
+  }
+  for (int i=0;i<length;i++){
+
+    if (pos[i]>max)
+      max = pos[i];
+
+    if (pos[i]<min)
+      min = pos[i];  
+  }
+  if (strArr[0][0]==strArr[0][1] && strArr[1].find(strArr[0][0])){
+    int tempCount = 0;
+    for (int i=0;i<length;i++){
+
+      if (strArr[1][strArr[1].find(strArr[0][0])]==strArr[1][i])
+        tempCount++;
+    }
+    if (tempCount == 1)
+      min=1;
+  }
+  temp = strArr[0].substr(min,max);
   // code goes here  
-  cout << arrLength << endl;
-  return strArr[0];
+  return temp;
 
 }
 
